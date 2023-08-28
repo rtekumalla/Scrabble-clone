76fc39bf60358ddfb5c031c5554557d9ad657960
rtekumalla <129798345+rtekumalla@users.noreply.github.com>
2023-08-28 16:04:15 -0700

    Letters
    A list of Letter tiles and a dictionary that sets each tile to its respective point value

---
+++ Scrabble_letters.py
@@ -0,0 +1,40 @@
+# A list that has the letter tiles that are used in every game of Scrabble
+
+letter_tiles = [" ", " ", "a", "a", "a", "a", "a", "a", "a", "a", "a", "b", "b", "c", "c", "d", "d", "d", "d", "e", "e",
+                "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "f", "f", "g", "g", "g", "h", "h", "i", "i", "i", "i",
+                "i", "i", "i", "i", "i", "j", "k", "l", "l", "l", "l", "m", "m", "n", "n", "n", "n", "n", "n", "o", "o",
+                "o", "o", "o", "o", "o", "o", "p", "p", "q", "r", "r", "r", "r", "r", "r", "s", "s", "s", "s", "t", "t",
+                "t", "t", "t", "t", "u", "u", "u", "u", "v", "v", "w", "w", "x", "y", "y", "z"
+                ]
+
+# A dictionary that assigns the point value of each tile to their respective letter
+
+tile_values = {
+    " ": 0,
+    "a": 1,
+    "b": 3,
+    "c": 3,
+    "d": 2,
+    "e": 1,
+    "f": 4,
+    "g": 2,
+    "h": 4,
+    "i": 1,
+    "j": 8,
+    "k": 5,
+    "l": 1,
+    "m": 3,
+    "n": 1,
+    "o": 1,
+    "p": 3,
+    "q": 10,
+    "r": 1,
+    "s": 1,
+    "t": 1,
+    "u": 1,
+    "v": 4,
+    "w": 4,
+    "x": 8,
+    "y": 4,
+    "z": 10,
+}
